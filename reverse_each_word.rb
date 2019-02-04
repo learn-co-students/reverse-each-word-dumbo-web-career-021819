7f43199b0f46f12990d4758ad9e830b72302f5cd
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 15:08:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,8 @@ def reverse_each_word(str)
   words = str.split().collect { |word| word.reverse }
   words.join(" ")
 end
+
+## witout iteration
+def reverse_each_word_1(sentence)
+  sentence.reverse.split.reverse.join(" ")
+end
